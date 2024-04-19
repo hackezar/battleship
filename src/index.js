d9617bb11477e976838f90a2419bbce42ed0fdf6
hackezar <107069171+hackezar@users.noreply.github.com>
2024-04-19 15:17:10 -0400

    This Commit Will be moved to a new repo the new repo is testing practice

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import _ from "lodash";
 import "./style.css";
 
-console.log("Hello");
+import { analyzeArray, caesarCipher } from "./testingPractice.js";
+
+console.log(caesarCipher('jack halcisak!?', 3));
+
+console.log(analyzeArray([1, 8, 3, 4, 2, 6]));
