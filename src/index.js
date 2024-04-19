1652d1712d9dcb5316247becd8cc15e0d06bdf08
hackezar <107069171+hackezar@users.noreply.github.com>
2024-04-19 15:35:44 -0400

    Added jest and testing capabilities to the template
    Everything else works as before

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import _ from "lodash";
 import "./style.css";
 
-import { analyzeArray, caesarCipher } from "./testingPractice.js";
+import { capitalizeFirst } from "./testingPractice";
 
-console.log(caesarCipher('jack halcisak!?', 3));
-
-console.log(analyzeArray([1, 8, 3, 4, 2, 6]));
+console.log('hello');
